59220cfbac392c55ea20e48d41de8a39f8e3de4d
Nouran <nouranha122@gmail.com>
2025-04-10 16:49:39 +0200

    test1

---
+++ main.py
@@ -0,0 +1,17 @@
+#import os
+#import requests
+#from dotenv import load_dotenv
+
+# Lade .env-Variablen
+#load_dotenv()
+
+#webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
+
+#data = {
+ #   "content": "👋 Hello from YoBot! Ich bin live im Discord!"
+#}
+
+#response = requests.post(webhook_url, json=data)
+
+#print("Status:", response.status_code)
+#print("Antwort:", response.text)
